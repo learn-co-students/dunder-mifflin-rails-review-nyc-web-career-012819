9642b64e4591861a37e02e55f797108b655c6fd3
stomg7969 <stomg7969@gmail.com>
2019-03-01 09:04:33 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  # def self.number_of_employees_owned
+  #   # self.employees.count
+  #   Dog.all.map do |emp|
+  #     emp.employees.count
+  #   end
+  # end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,14 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, :presence => true
+  validates :alias, :uniqueness => true
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  # having trouble displyaing error message
+  # also the bonus: sorting by number of employees the dog owned in new.erb
+
+  def to_s
+    self.first_name + ', ' + self.last_name.upcase
+  end
 end
